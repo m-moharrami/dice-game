5855cde50d13ed9f6087b293f5524629c256a614
Moharram Moharrami <moharram.m98@gmail.com>
2021-11-11 20:16:43 +0330

    show players scores in their scoreboards

--- index.js
+++ index.js
@@ -16,11 +16,15 @@ rollButton.addEventListener('click', function() {
 
     if (player1Turn) {
         player1Dice.textContent = result;
+        player1Score += result;
+        player1Scoreboard.textContent = player1Score;
         player1Dice.classList.remove("active");
         player2Dice.classList.add("active");
         message.textContent = "Player 2 turn";
     } else {
         player2Dice.textContent = result;
+        player2Score += result;
+        player2Scoreboard.textContent = player2Score;
         player2Dice.classList.remove("active");
         player1Dice.classList.add("active");
         message.textContent = "Player 1 turn";
